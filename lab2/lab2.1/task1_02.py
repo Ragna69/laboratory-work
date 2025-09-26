31b1a7f2b5bf9eb381cc55264d019fbb4b32ee6e
Макс <svetachapa1963@gmail.com>
2025-09-26 17:40:50 +0300

    Добавлен код в задания lab2.1

--- lab2/lab2.1/task1_02.py
+++ lab2/lab2.1/task1_02.py
@@ -1,1 +1,19 @@
+chisla = input('Введите числа:\n')
+digits  = chisla.split()
+
+spisok = {}
+for digit in digits:
+    spisok[digit] = spisok.get(digit, 0) + 1;
+
+# print('Список слов:')
+# for digit in spisok:
+#     print(digit,':', spisok[digit])
+
+unikalnie = len(spisok)
+print('Количество уникальных чисел:', unikalnie)
+
+print('Повторяющиеся числа:')
+for digit in spisok:
+    if spisok[digit] > 1:
+        print(digit,':', spisok[digit])
 
