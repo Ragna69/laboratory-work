4fa4a3040d943ca67f3e70c4798414b3e74bc93c
Макс <svetachapa1963@gmail.com>
2025-09-26 19:28:43 +0300

    Анализ списка чисел

--- lab2/lab2.1/task1_02.py
+++ lab2/lab2.1/task1_02.py
@@ -5,15 +5,79 @@ spisok = {}
 for digit in digits:
     spisok[digit] = spisok.get(digit, 0) + 1;
 
-# print('Список слов:')
-# for digit in spisok:
-#     print(digit,':', spisok[digit])
-
 unikalnie = len(spisok)
-print('Количество уникальных чисел:', unikalnie)
+print('Количество уникальных чисел:\n', unikalnie)
 
 print('Повторяющиеся числа:')
+found = False
 for digit in spisok:
     if spisok[digit] > 1:
         print(digit,':', spisok[digit])
+        found = True
+if not found:
+    print('0')
+
+print('Четные числа:')
+found = False
+for digit in spisok:
+    if not '-' or not '.' in digit:
+        if int(digit) % 2 == 0:
+           print(digit)
+           found = True
+if not found:
+    print('0')
+
+print('Нечетные числа:')
+found = False
+for digit in spisok:
+    if not '-' or not '.' in digit:
+       if int(digit) % 2 == 1:
+           print(digit)
+           found = True
+if not found:
+    print('0')
+
+print('Отрицательные числа:')
+found = False
+for digit in spisok:
+    if '-' in digit:
+        print(digit)
+        found = True
+if not found:
+    print('0')
+
+print('Числа с плавающей точкой:')
+found = False
+for digit in spisok:
+    if '.' in digit or 'e' in digit.lower():
+        print(digit)
+        found = True
+if not found:
+    print('0')
+
+print('Сумма всех чисел, кратных 5:')
+found = False
+summa = sum(int(digit) for digit in digits if digit.lstrip('-').isdigit() and int(digit) % 5 ==0)
+if summa:
+    print(summa)
+    found = True
+if not found:
+    print('0')
+
+largest = [float(digit) for digit in digits if digit.lstrip('-').replace('.','',1).isdigit()]
+if largest:
+    largest = max(largest, key=float)
+    print('Самое большое число:\n', largest)
+else:
+    print('Нет таких чисел')
+
+
+minimal = [float(digit) for digit in digits if digit.lstrip('-').replace('.','',1).isdigit()]
+if minimal:
+    smallest = min(minimal)
+    print('Самое маленькое число:\n', smallest)
+else:
+    print('Нет таких чисел')
+
+
 
