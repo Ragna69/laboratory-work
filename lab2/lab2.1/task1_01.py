982cfe5ab5c15a71fa6db7f957b5b3cd45f8879f
Макс <svetachapa1963@gmail.com>
2025-09-26 17:38:24 +0300

    Посчитать слова

--- lab2/lab2.1/task1_01.py
+++ lab2/lab2.1/task1_01.py
@@ -1,1 +1,20 @@
-print('HELLO')
+# text = "   это   пример   строки   "
+# words = text.split()
+# print(words)  # ['это', 'пример', 'строки']
+
+text = input('Введите текст')
+words  = text.lower().split()
+
+spisok = {}
+for word in words:
+    spisok[word] = spisok.get(word, 0) + 1;
+
+print('Список слов:')
+for word in spisok:
+    print(word, spisok[word])
+
+unikalnie = len(spisok)
+print('Количество уникальных слов:', str(unikalnie))
+
+
+
