55be088055453cd7cdbdba586f2d56e15ac54a50
Макс <svetachapa1963@gmail.com>
2025-09-27 19:14:35 +0300

    backup: перед переименованием папок

--- lab2/lab2.1/task1_06.py
+++
@@ -1,9 +0,0 @@
-#Задача 6. Удаление дубликатов без set()
-
-word_list = input('Введите список символов:\n').strip().split()
-
-unique = []
-for word in word_list:
-    if word not in unique:
-        unique.append(word) # .appent() - добавляет элемент в конец списка
-print('Список без повторяющихся символов:', *unique)
