ce18af899f913a3ebc294e7b30ccdd1abc8e3b16
Макс <svetachapa1963@gmail.com>
2025-09-27 12:08:35 +0300

    Аннаграммы

--- lab2/lab2.1/task1_05.py
+++ lab2/lab2.1/task1_05.py
@@ -1,1 +1,11 @@
+word_1 = input('Введите первое слово:\n')
+word_2 = input('Введите второе слово:\n')
+
+word1 = list(word_1)
+word2 = list(word_2)
+
+if sorted(word1) == sorted(word2):
+    print('Это аннаграммы!')
+else:
+    print('Это не аннаграммы.')
 
