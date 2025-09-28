59cc4e1da468bc151f1cb2dbe556d92519c5a166
Макс <svetachapa1963@gmail.com>
2025-09-28 18:03:33 +0300

    Слияние

--- lab2/lab_2_2/task2_02.py
+++ lab2/lab_2_2/task2_02.py
@@ -1,1 +1,27 @@
+# 2)  Слияние
 
+def merge(a, b):
+    for key in b:
+        if key in a:
+            # оба словари - рекурсивно объединяет
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge(a[key], b[key])
+            # оба списки - объединяет без повторов
+            elif isinstance(a[key], list) and isinstance(b[key], list):
+                merge(a[key], b[key])
+            # оба множества - объединяет
+            elif isinstance(a[key], set) and isinstance(b[key], set):
+                merge(a[key], b[key])
+            # оба кортежи - объединяет и превращвет в кортеж
+            elif isinstance(a[key], tuple) and isinstance(b[key], tuple):
+                merge(a[key], b[key])
+            else:
+               a[key] = b[key]
+        else:
+            a[key] = b[key] # в a нет ключа - добавить
+
+dict_a = {"a": 1, "b":{"c": 1, "f": 4}}
+dict_b = {"d": 1, "b":{"c": 2, "e": 3}}
+
+merge(dict_a, dict_b)
+print(dict_a)
