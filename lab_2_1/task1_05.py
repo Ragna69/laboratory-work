b08f0c8198e7ac313b1dd5c69d891c79af537577
Макс <svetachapa1963@gmail.com>
2025-09-29 00:50:25 +0300

    Анаграммы

--- lab_2_1/task1_05.py
+++ lab_2_1/task1_05.py
@@ -6,8 +6,8 @@ word_2 = input('Введите второе слово:\n')
 word1 = list(word_1)
 word2 = list(word_2)
 
-if sorted(word1) == sorted(word2):# ┬─ sorted - text = "python"
-#                                   └─ print(sorted(text))  # ['h', 'n', 'o', 'p', 't', 'y']
+if sorted(word1) == sorted(word2): # ┬─ sorted - text = "python"
+#                                    └─ print(sorted(text))  # ['h', 'n', 'o', 'p', 't', 'y']
     print('Это анаграммы!')
 else:
     print('Это не анаграммы.')
