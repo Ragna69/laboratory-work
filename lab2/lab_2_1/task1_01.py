30a0d2c34021a30df9f8897267e9dd676276c10d
Макс <svetachapa1963@gmail.com>
2025-09-28 14:06:10 +0300

    Посчитать слова.

--- lab2/lab_2_1/task1_01.py
+++ lab2/lab_2_1/task1_01.py
@@ -5,7 +5,7 @@ words  = text.lower().split()
 
 spisok = {}
 for word in words:
-    spisok[word] = spisok.get(word, 0) + 1  # добавляет счетчик к слову
+    spisok[word] = spisok.get(word, 0) + 1   # добавляет счетчик к слову
 print('Список слов:')
 for word in spisok:
     print(word, spisok[word])
