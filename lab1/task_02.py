a10a4984bf93e9db90de7b9ec63e07942d122b1f
Макс <svetachapa1963@gmail.com>
2025-09-29 16:16:45 +0300

    Удаление гласных (правка)

--- lab1/task_02.py
+++ lab1/task_02.py
@@ -2,7 +2,7 @@ s = input('Введите строку:')
 if not s:
     print('Пустая строка')
 else:
-        g = "aeiouAEIOUуеыаоэяию"
+        g = "aeiou" # Введите строку и удалите из нее все гласные (a, e, i, o, u), затем выведите результат.
         result = ""
         for char in s:
             if char not in g:
