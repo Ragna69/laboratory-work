853fa9c8c07da39efe4b3b677b4a697633745158
Макс <svetachapa1963@gmail.com>
2025-11-01 18:54:33 +0300

    График

---
+++ lab4.1/task2.py
@@ -0,0 +1,45 @@
+# import matplotlib.pyplot as plt
+# import math
+#
+# x_range = range(-10,10)
+# x_filter = [x for x in x_range if x != 3 and x != -3]
+# f_x = [ 5/(x ** 2 - 9)
+# for x in x_filter]
+#
+#
+#
+# plt.figure(figsize=(10, 10))
+# plt.plot(x_filter,f_x, label='f(x)', color = 'black')
+#
+# plt.title('График функции на интервале [−10; 10]')
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
+# plt.grid(True)
+# plt.legend()
+# plt.show()
+
+import matplotlib.pyplot as plt
+import math
+
+x_range = [x * 0.01 for x in range(-1000, 1000) ] #if x != 3 and x != -3
+
+f_x = [1 / (x**2 - 9) if (x**2 - 9) != 0
+       else math.nan #math.nan - не число, нужно для разрывов
+    for x in x_range]
+
+plt.figure(figsize=(10, 10))
+plt.plot(x_range, f_x, color='black', label='f(x) = 1 / (x² - 9)')
+
+# вертикальные асимптоты
+plt.axvline(x=3, color='gray', linestyle='--')
+plt.axvline(x=-3, color='gray', linestyle='--')
+
+# горизонтальная асимптота
+plt.axhline(y=0, color='black', linestyle=':')
+
+plt.title('График функции f(x) = 1 / (x² - 9)')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.grid(True)
+plt.legend()
+plt.show()
