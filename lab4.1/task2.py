78712277df3681a22f3a1b5f700d914e1495c55e
Макс <svetachapa1963@gmail.com>
2025-11-04 13:32:39 +0300

    Изменил

--- lab4.1/task2.py
+++ lab4.1/task2.py
@@ -3,12 +3,12 @@ import math
 
 x_range = [x * 0.01 for x in range(-1000, 1000) ] #if x != 3 and x != -3
 
-f_x = [1 / (x**2 - 9) if (x**2 - 9) != 0
+f_x = [5 / (x**2 - 9) if (x**2 - 9) != 0
        else math.nan #math.nan - не число, нужно для разрывов
     for x in x_range]
 
 plt.figure(figsize=(10, 10))
-plt.plot(x_range, f_x, color='black', label='f(x) = 1 / (x² - 9)')
+plt.plot(x_range, f_x, color='black', label='f(x) = 5 / (x² - 9)')
 
 # вертикальные асимптоты
 plt.axvline(x=3, color='gray', linestyle='--')
@@ -17,7 +17,7 @@ plt.axvline(x=-3, color='gray', linestyle='--')
 # горизонтальная асимптота
 plt.axhline(y=0, color='black', linestyle=':')
 
-plt.title('График функции f(x) = 1 / (x² - 9)')
+plt.title('График функции f(x) = 5 / (x² - 9)')
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.grid(True)
