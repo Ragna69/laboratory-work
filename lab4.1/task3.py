4fdd86c994c17432d00db250af15fda3bd94f1c6
Макс <svetachapa1963@gmail.com>
2025-11-06 00:33:35 +0300

    Доделал черного кота

--- lab4.1/task3.py
+++ lab4.1/task3.py
@@ -1,57 +1,107 @@
-# import matplotlib.pyplot as plt
-#
-# plt.figure(figsize=(10, 10))
-#
-# from matplotlib.patches import Circle, Polygon, Rectangle
-# fig, ax = plt.subplots()
-# ax.circle(0, 0, 1, color='red')
-# plt.title('Рисунок ...')
-# plt.legend()
-# plt.show()
-
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle, Polygon, Rectangle
 
 fig, ax = plt.subplots(figsize=(100, 100))
 
-# Добавляем круг с центром (0, 0) и радиусом 1
+ax.set_xlim(-10, 8.2)
+ax.set_ylim(-7.9, 10)
 
-#68 треугольников
+# Для кота
 color1  = (0.00, 0.00, 0.00)  # Чистый чёрный
 color2  = (0.035, 0.035, 0.035)  # Очень тёмный
 color3  = (0.07, 0.07, 0.07)  # Почти угольный
 color4  = (0.105, 0.105, 0.105)  # Графитовый
 color5  = (0.14, 0.14, 0.14)  # Абсолютный чёрный
-color6  = (0.175, 0.175, 0.175)  # Тёмно-серый
-color7  = (0.21, 0.21, 0.21)  # Серый с оттенком угля
-color8  = (0.245, 0.245, 0.245)  # Серый
-color9  = (0.28, 0.28, 0.28)  # Мягкий серый
-color10 = (0.315, 0.315, 0.315)  # Теплый серый
-color11 = (0.35, 0.35, 0.35)  # Светло-серый
-color12 = (0.385, 0.385, 0.385)  # Почти светлый
-color13 = (0.42, 0.42, 0.42)  # Серый с серебром
-color14 = (0.455, 0.455, 0.455)  # Серебристый
-color15 = (0.50, 0.50, 0.50)  # Средний серый
-color16 = (0.9, 0.9, 0.9)  # Почти белый
 
+violet1 = (0.1, 0.05, 0.2, 1.0)   # Тёмно-фиолетовый (почти чёрный)
+violet2 = (0.2, 0.1, 0.4, 1.0)    # Глубокий индиго-фиолетовый
+violet3 = (0.3, 0.15, 0.6, 1.0)   # Холодный фиолетовый
+violet4 = (0.4, 0.2, 0.7, 1.0)    # Классический фиолетовый
+violet5 = (0.5, 0.3, 0.8, 1.0)    # Яркий фиолетовый
+violet6 = (0.6, 0.4, 0.9, 1.0)    # Светло-фиолетовый
+violet7 = (0.7, 0.5, 1.0, 1.0)    # Сиреневый
+
+# Для фона
+violet8 = (0.3, 0.15, 0.6, 0.6)    # Светлый холодный фиолетовый
+violet9 = (0.3, 0.15, 0.6, 0.5)   # Светлый холодный фиолетовый
+violet10 = (0.4, 0.2, 0.7, 0.5)    # Светлый классический фиолетовый
+violet11 = (0.5, 0.3, 0.8, 0.5)    # Светлый яркий фиолетовый
+violet12 = (0.6, 0.4, 0.9, 0.5)    # Светлый светло-фиолетовый
+violet13 = (0.7, 0.5, 1.0, 0.5)    # Светло-сиреневый
+violet14 = (0.7, 0.5, 1.0, 0.6)    # Светло-сиреневый
+
+# Фон
 #1
-ax.add_patch(Polygon([[-9.6, 9.6], [-4, 6.4], [-5.8, 4.6]], closed=True, color=color6))
+ax.add_patch(Polygon([[-10, 10], [-10, 3], [-5.8, 10]], closed=True, color=violet11))
+ax.add_patch(Polygon([[-10, 10], [-10, 3], [-5.8, 10]], closed=True, color=violet11))
+ax.add_patch(Polygon([[-10, 10], [-10, 3], [-5.8, 10]], closed=True, color=violet12))
+#2
+ax.add_patch(Polygon([[-5, -1], [-10, 3], [-5.8, 10]], closed=True, color=violet12))
+ax.add_patch(Polygon([[-5, -1], [-10, 3], [-5.8, 10]], closed=True, color=violet12))
+ax.add_patch(Polygon([[-5, -1], [-10, 3], [-5.8, 10]], closed=True, color=violet11))
+#3
+ax.add_patch(Polygon([[-5, -1], [-10, 3], [-10, -3]], closed=True, color=violet10))
+ax.add_patch(Polygon([[-5, -1], [-10, 3], [-10, -3]], closed=True, color=violet10))
+ax.add_patch(Polygon([[-5, -1], [-10, 3], [-10, -3]], closed=True, color=violet10))
+#4
+ax.add_patch(Polygon([[-10, -7.9], [-2, -7.9], [-10, -3]], closed=True, color=violet8))
+ax.add_patch(Polygon([[-10, -7.9], [-2, -7.9], [-10, -3]], closed=True, color=violet8))
+ax.add_patch(Polygon([[-10, -7.9], [-2, -7.9], [-10, -3]], closed=True, color=violet8))
+#5
+ax.add_patch(Polygon([[-5, -1], [-2, -7.9], [-10, -3]], closed=True, color=violet8))
+ax.add_patch(Polygon([[-5, -1], [-2, -7.9], [-10, -3]], closed=True, color=violet9))
+ax.add_patch(Polygon([[-5, -1], [-2, -7.9], [-10, -3]], closed=True, color=violet9))
+#6
+ax.add_patch(Polygon([[6, 0], [-2, -7.9], [-5, -1]], closed=True, color=violet9))
+ax.add_patch(Polygon([[6, 0], [-2, -7.9], [-5, -1]], closed=True, color=violet9))
+ax.add_patch(Polygon([[6, 0], [-2, -7.9], [-5, -1]], closed=True, color=violet10))
+#7
+ax.add_patch(Polygon([[-5.8, 10], [-5, -1], [4, 2]], closed=True, color=violet11))
+ax.add_patch(Polygon([[-5.8, 10], [-5, -1], [4, 2]], closed=True, color=violet11))
+ax.add_patch(Polygon([[-5.8, 10], [-5, -1], [4, 2]], closed=True, color=violet13))
+#8
+ax.add_patch(Polygon([[6, 0], [-2, -7.9], [4, -7.9]], closed=True, color=violet9))
+ax.add_patch(Polygon([[6, 0], [-2, -7.9], [4, -7.9]], closed=True, color=violet10))
+ax.add_patch(Polygon([[6, 0], [-2, -7.9], [4, -7.9]], closed=True, color=violet10))
+#9
+ax.add_patch(Polygon([[6, 0], [8.2, -7.9], [4, -7.9]], closed=True, color=violet11))
+ax.add_patch(Polygon([[6, 0], [8.2, -7.9], [4, -7.9]], closed=True, color=violet11))
+#10
+ax.add_patch(Polygon([[6, 0], [8.2, -7.9], [8.2, 3]], closed=True, color=violet12))
+ax.add_patch(Polygon([[6, 0], [8.2, -7.9], [8.2, 3]], closed=True, color=violet12))
+#11
+ax.add_patch(Polygon([[6, 0], [3, 6.5], [8.2, 3]], closed=True, color=violet13))
+ax.add_patch(Polygon([[6, 0], [3, 6.5], [8.2, 3]], closed=True, color=violet13))
+#12
+ax.add_patch(Polygon([[-2, 5], [3, 6.5], [2, 10]], closed=True, color=violet13))
+ax.add_patch(Polygon([[-2, 5], [3, 6.5], [2, 10]], closed=True, color=violet13))
+#13
+ax.add_patch(Polygon([[-1.08, 6.15], [-5.8, 10], [2, 10]], closed=True, color=violet12))
+ax.add_patch(Polygon([[-1.08, 6.15], [-5.8, 10], [2, 10]], closed=True, color=violet12))
+#14
+ax.add_patch(Polygon([[8.2, 10], [3, 6.5], [8.2, 3]], closed=True, color=violet14))
+#15
+ax.add_patch(Polygon([[8.2, 10], [3, 6.5], [2, 10]], closed=True, color=violet14))
+
+# Кот
+#1
+ax.add_patch(Polygon([[-9.6, 9.6], [-4, 6.4], [-5.8, 4.6]], closed=True, color=color4))
 #2
 ax.add_patch(Polygon([[-9.6, 9.6], [-8.4, 2.5], [-5.8, 4.6]], closed=True, color=color1))
 #3
-ax.add_patch(Polygon([[-4, 6.4], [0,6.4], [-5.8, 4.6]], closed=True, color=color4))
+ax.add_patch(Polygon([[-4, 6.4], [0,6.4], [-5.8, 4.6]], closed=True, color=color3))
 #4
-ax.add_patch(Polygon([[-3, 4], [0,6.4], [-5.8, 4.6]], closed=True, color=color3))
+ax.add_patch(Polygon([[-3, 4], [0,6.4], [-5.8, 4.6]], closed=True, color=color2))
 #5
-ax.add_patch(Polygon([[-3, 4], [-8.4, 2.5], [-5.8, 4.6]], closed=True, color=color1))
+ax.add_patch(Polygon([[-6.7, 2.3], [-8.4, 2.5], [-5.8, 4.6]], closed=True, color=color1))
 #6
-ax.add_patch(Polygon([[-6.7, 2.3], [-8.4, 2.5], [-3, 4]], closed=True, color=color1))
+ax.add_patch(Polygon([[-3, 4], [-6.7, 2.3], [-5.8, 4.6]], closed=True, color=color3))
 #7
-ax.add_patch(Polygon([[-3, 4], [0,6.4], [-1.2, 3]], closed=True, color=color2))
+ax.add_patch(Polygon([[-3, 4], [0,6.4], [-1.2, 3]], closed=True, color=color1))
 #8
-ax.add_patch(Polygon([[0.8, 4.5], [0,6.4], [-1.2, 3]], closed=True, color=color3))
+ax.add_patch(Polygon([[0.8, 4.5], [0,6.4], [-1.2, 3]], closed=True, color=color4))
 #9
-ax.add_patch(Polygon([[0.8, 4.5], [3.1,5.5], [0,6.4]], closed=True, color=color2))
+ax.add_patch(Polygon([[0.8, 4.5], [3.1,5.5], [0,6.4]], closed=True, color=color3))
 #10
 ax.add_patch(Polygon([[2.8,6.8], [3.1,5.5], [0,6.4]], closed=True, color=color5))
 #12
@@ -59,7 +109,7 @@ ax.add_patch(Polygon([[7.8, 9.6], [2.8,6.8], [3.1,5.5]], closed=True, color=colo
 #13
 ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
 #14
-ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[3.7, 1.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color1))
 #15
 ax.add_patch(Polygon([[-6.7, 2.3], [-8.4, 2.5], [-9, -0.8]], closed=True, color=color1))
 #16
@@ -69,9 +119,9 @@ ax.add_patch(Polygon([[-4.2, 0.9], [-6.7, 2.3], [-6.4, 0.5]], closed=True, color
 #18
 ax.add_patch(Polygon([[-6.7, 2.3], [-4.2, 0.9], [-3, 4]], closed=True, color=color4))
 #19
-ax.add_patch(Polygon([[-9, -0.8], [-6.4, 0.5], [-5, -1.6]], closed=True, color=color4))
+ax.add_patch(Polygon([[-8.4, -3.6], [-6.4, 0.5], [-5, -1.6]], closed=True, color=color2))
 #20
-ax.add_patch(Polygon([[-9, -0.8], [-8.4, -3.6], [-5, -1.6]], closed=True, color=color1))
+ax.add_patch(Polygon([[-9, -0.8], [-8.4, -3.6], [-6.4, 0.5]], closed=True, color=color1))
 #21
 ax.add_patch(Polygon([[-4.8, -4.7], [-8.4, -3.6], [-5, -1.6]], closed=True, color=color1))
 #22
@@ -79,25 +129,28 @@ ax.add_patch(Polygon([[-4.8, -4.7], [-8.4, -3.6], [-7, -5.6]], closed=True, colo
 #23
 ax.add_patch(Polygon([[-4.8, -4.7], [-4.3, -6.4], [-7, -5.6]], closed=True, color=color2))
 #24
-ax.add_patch(Polygon([[-4.8, -4.7], [-4.3, -6.4], [-1.2, -5.2]], closed=True, color=color2))
+ax.add_patch(Polygon([[-4.8, -4.7], [-4.3, -6.4], [-1.2, -5.2]], closed=True, color=color3))
 #25
 ax.add_patch(Polygon([[-4.8, -4.7], [-2.6, -3.6], [-1.2, -5.2]], closed=True, color=color4))
 #26
 ax.add_patch(Polygon([[-4.8, -4.7], [-2.6, -3.6], [-3, -0.8]], closed=True, color=color1))
 #27
 ax.add_patch(Polygon([[-4.8, -4.7], [-5, -1.6], [-3, -0.8]], closed=True, color=color2))
-#28
-ax.add_patch(Polygon([[-5, -0.1], [-6.4, 0.5], [-5, -1.6]], closed=True, color=color1))
-#29
-ax.add_patch(Polygon([[-5, -0.1], [-6.4, 0.5], [-4.2, 0.9]], closed=True, color=color1))
-#30
-ax.add_patch(Polygon([[-4, -0.4], [-5, -1.6], [-3, -0.8]], closed=True, color=color1))
-#31
-ax.add_patch(Polygon([[-4, -0.4], [-3, -0.8], [-4.2, 0.9]], closed=True, color=color2))
-#32
-#ax.add_patch(Polygon([[-4, -0.4], [-5, -0.1], [-4.2, 0.9]], closed=True, color=color16))
-#33
-#ax.add_patch(Polygon([[-4, -0.4], [-5, -0.1], [-5, -1.6]], closed=True, color=color16))
+
+#28 левый зрачок
+ax.add_patch(Polygon([[-5, -0.1],[-4, -0.4] , [-4.2, 0.9]], closed=True, color=color2))
+#29 левый зрачок
+ax.add_patch(Polygon([[-5, -0.1],[-4, -0.4], [-5, -1.6]], closed=True, color=color1))
+
+#30 левый глаз
+ax.add_patch(Polygon([[-5, -0.1], [-6.4, 0.5], [-4.2, 0.9]], closed=True, color=violet2))
+#31 левый глаз
+ax.add_patch(Polygon([[-4, -0.4], [-5, -1.6], [-3, -0.8]], closed=True, color=violet3))
+#32 левый глаз
+ax.add_patch(Polygon([[-5, -0.1], [-6.4, 0.5], [-5, -1.6]], closed=True, color=violet1))
+#33 левый глаз
+ax.add_patch(Polygon([[-4, -0.4], [-3, -0.8], [-4.2, 0.9]], closed=True, color=violet6))
+
 #34
 ax.add_patch(Polygon([[-3, 4], [-3, -0.8], [-4.2, 0.9]], closed=True, color=color1))
 #35
@@ -113,65 +166,62 @@ ax.add_patch(Polygon([[0.2, -3.6], [-1.2, -1.1], [-2.6, -3.6]], closed=True, col
 #40
 ax.add_patch(Polygon([[0.2, -3.6], [-1.2, -5.2], [-2.6, -3.6]], closed=True, color=color1))
 #41.
-ax.add_patch(Polygon([[2.4, -4.7], [0.2, -3.6], [-1.2, -5.2]], closed=True, color=color4))
+ax.add_patch(Polygon([[2.4, -4.7], [0.2, -3.6], [-1.2, -5.2]], closed=True, color=color5))
 #42
-ax.add_patch(Polygon([[2.4, -4.7], [2, -6.7], [-1.2, -5.2]], closed=True, color=color3))
+ax.add_patch(Polygon([[2.4, -4.7], [2, -6.7], [-1.2, -5.2]], closed=True, color=color4))
 #43
-ax.add_patch(Polygon([[-1.1, -7.8], [2, -6.7], [-1.2, -5.2]], closed=True, color=color1))
+ax.add_patch(Polygon([[-1.1, -7.5], [2, -6.7], [-1.2, -5.2]], closed=True, color=color1))
 #44
-ax.add_patch(Polygon([[-1.1, -7.8], [-4.3, -6.4], [-1.2, -5.2]], closed=True, color=color1))
+ax.add_patch(Polygon([[-1.1, -7.5], [-4.3, -6.4], [-1.2, -5.2]], closed=True, color=color1))
 #45
-ax.add_patch(Polygon([[2.4, -4.7], [0.2, -3.6], [0.7, -1.2]], closed=True, color=color2))
+ax.add_patch(Polygon([[2.4, -4.7], [0.2, -3.6], [0.5, -0.8]], closed=True, color=color2))
 #46
-ax.add_patch(Polygon([[-1.2, 3], [0.2, -3.6], [0.7, -1.2]], closed=True, color=color3))
+ax.add_patch(Polygon([[-1.2, 3], [0.2, -3.6], [0.5, -0.8]], closed=True, color=color3))
 #47
-
+ax.add_patch(Polygon([[-1.2, 3], [1.9, 0.7], [0.5, -0.8]], closed=True, color=color2))
 #48
-
+ax.add_patch(Polygon([[0.8, 4.5],[1.9, 0.7] , [-1.2, 3]], closed=True, color=color5))
 #49
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[0.8, 4.5],[1.9, 0.7] ,[3.1, 5.5] ], closed=True, color=color2))
 #50
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[3.7, 1.6],[1.9, 0.7] ,[3.1, 5.5] ], closed=True, color=color3))
 #51
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[3.7, 1.6],[1.9, 0.7] ,[4.1, 0.20 ]], closed=True, color=color4))
 #52
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
-#53
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
-#54
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
-#55
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
-#56
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[2.4, -4.7], [2.7, -1.7], [0.5, -0.8]], closed=True, color=color3))
+
+#53 правый зрачок
+ax.add_patch(Polygon([[1.5, -0.45], [1.9, 0.7],[2.55,-0.2] ], closed=True, color=color2))
+#54 правый зрачок
+ax.add_patch(Polygon([[1.5, -0.45], [2.7, -1.7],[2.55,-0.2] ], closed=True, color=color1))
+
+#55 правый глаз
+ax.add_patch(Polygon([[1.5, -0.45], [1.9, 0.7], [0.5, -0.8]], closed=True, color=violet6))
+#56 правый глаз
+ax.add_patch(Polygon([[1.5, -0.45], [2.7, -1.7], [0.5, -0.8]], closed=True, color=violet3))
+#58 правый глаз
+ax.add_patch(Polygon([[2.55, -0.2],[1.9, 0.7]  , [4.1,0.2]], closed=True, color=violet2))
+#57 правый глаз
+ax.add_patch(Polygon([[2.55, -0.2], [2.7, -1.7], [4.1,0.2]], closed=True, color=violet1))
+
 #57
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[3.7, 1.6], [6.9, 0.8], [6.4, 3.7]], closed=True, color=color2))
 #58
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[3.7, 1.6], [6.9, 0.8], [4.1,0.2]], closed=True, color=color3))
 #59
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[2.7, -1.7], [6.9, 0.8], [4.2, -3]], closed=True, color=color4))
 #60
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[2.7, -1.7], [6.9, 0.8], [4.1,0.25]], closed=True, color=color3))
 #61
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[2.7, -1.7],[2.4, -4.7] , [4.2, -3]], closed=True, color=color2))
 #62
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[2, -6.7], [2.4, -4.7] , [4.2, -3]], closed=True, color=color3))
 #63
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[6.8, -2.6],[6.9, 0.8] , [4.2, -3]], closed=True, color=color2))
 #64
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
+ax.add_patch(Polygon([[6.8, -2.6],[5, -5] , [4.2, -3]], closed=True, color=color1))
 #65
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
-#66
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
-#67
-#ax.add_patch(Polygon([[7.8, 9.6], [3.1,5.5], [6.4, 3.7]], closed=True, color=color4))
-#68
-
-
-
-ax.set_xlim(-10, 10)
-ax.set_ylim(-10, 10)
+ax.add_patch(Polygon([[2, -6.7],[5, -5] , [4.2, -3]], closed=True, color=color2))
 
 plt.title('Рисунок кота')
 plt.grid(False)
