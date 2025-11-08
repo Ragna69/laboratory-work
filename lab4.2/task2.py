54d7f4a1149a9a05498a9e25f8e00e695acfc24d
Макс <svetachapa1963@gmail.com>
2025-11-08 18:13:22 +0300

    Дорога из пункта А в пункт В

--- lab4.2/task2.py
+++ lab4.2/task2.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# Входные данные
 lengths_str = "20 8 9 18 5 12 16 16 6 7"
 speeds_str = "44 70 44 66 46 38 38 37 66 67"
 k = 4
@@ -10,19 +9,15 @@ p = 7
 lengths = np.fromstring(lengths_str, sep=' ')
 speeds = np.fromstring(speeds_str, sep=' ')
 
-# Индексы участков: от k до p включительно → индексы k-1 до p
 segment_lengths = lengths[k-1:p]
 segment_speeds = speeds[k-1:p]
 
-# Общая длина пути
 S = np.sum(segment_lengths)
 
-# Время на каждом участке: длина / скорость
-times = segment_lengths / segment_speeds
-T = np.sum(times)
+time = segment_lengths / segment_speeds
+T = np.sum(time)
 
-# Средняя скорость: общая длина / общее время
+# Средняя скорость
 V = S / T
 
-# Вывод результатов
 print(f"S = {S:.0f} км, T = {T:.2f} час, V = {V:.2f} км/ч")
