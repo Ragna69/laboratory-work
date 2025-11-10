85ef1f5eca9de0b1d5dbf2d9e50ab9fd07f75fd3
Макс <svetachapa1963@gmail.com>
2025-11-10 20:38:06 +0300

    Сжатие строки

--- lab_2_1/task1_07.py
+++ lab_2_1/task1_07.py
@@ -17,12 +17,4 @@ else:
     print(compressed)
 
 
-# 2 способ (он лучше, потому что учитывает повторяющиеся символы в независимости от расположения)
-# from collections import Counter
-# word_list = input('Введите список символов:\n').strip().split()
-# #                           ┌─ from collections import Counter
-# counts = Counter(word_list)#┼─ counts = Counter('aaabbc')
-# #                           └─ {'a': 3, 'b': 2, 'c': 1}                  ┌─ ''.join() - склеивает элементы с разделителем м/ду ними.
-# compressed = ''.join(f'{char}{count}' for char, count in counts.items())#┼─ f'{char}{count}' - вызывает значения переменных char и count.
-# #                                                                        └─ count.items() - это метод словаря dict, который возвращает все пары ключ–значение.
-# print(compressed)
+
