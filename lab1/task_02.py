a97e85163d664159364b95162ff556874f7c80e7
Макс <svetachapa1963@gmail.com>
2025-11-22 18:23:14 +0300

    Delete lab1 directory

--- lab1/task_02.py
+++
@@ -1,6 +0,0 @@
-string = input('Введите строку:')
-if not string:
-    print('Пустая строка')
-else:
-        string = string.replace('a', '').replace('o', '').replace('i', '').replace('u', '').replace('e', '').replace('y', '')
-        print("Результат:", string)
