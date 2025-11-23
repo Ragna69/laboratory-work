57277e824320d17e5a1df7904015b017e9ab9913
Макс <svetachapa1963@gmail.com>
2025-11-23 18:16:27 +0300

    Поиск уникальных элементов

--- lab5.2/test_find_unique.py
+++ lab5.2/test_find_unique.py
@@ -1,23 +1,24 @@
 import pytest
 from task2 import find_unique
 
-def test_simple_list():
+def test_1():
     assert find_unique([1, 2, 2, 3, 4, 4]) == [1, 3]
 
-def test_all_unique():
+def test_2():
     assert find_unique([10, 20, 30]) == [10, 20, 30]
 
-def test_no_unique():
+def test_3():
     assert find_unique([5, 5, 6, 6, 7, 7]) == []
 
-def test_strings():
+def test_4():
     assert find_unique(["яблоко", "привет", "стол", "компьютер"]) == ["яблоко", "привет", "стол", "компьютер"]
 
-def test_mixed_types():
+def test_5():
     assert find_unique([1, "а", 2, "а", 3]) == [1, 2, 3]
 
-def test_empty_list():
+def test_6():
     assert find_unique([]) == []
 
-def test_single_element():
+def test_7():
     assert find_unique([52]) == [52]
+# pytest -v
