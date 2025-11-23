fd9e53bbdc5b0843f3779b9ae221a3c4418afe87
Макс <svetachapa1963@gmail.com>
2025-11-23 18:18:34 +0300

    Палиндром

---
+++ lab5.2/test_is_palindrome.py
@@ -0,0 +1,25 @@
+from task3 import is_palindrome   # task3.py — файл, где лежит функция
+def test_1():
+    assert is_palindrome("шалаш") is True
+
+def test_2():
+    assert is_palindrome("python") is False
+
+def test_3():
+    assert is_palindrome(12321) is True
+
+def test_4():
+    assert is_palindrome(12345) is False
+
+def test_5():
+    assert is_palindrome("БольшойСлон") is True
+
+def test_6():
+    assert is_palindrome("") is True
+
+def test_7():
+    assert is_palindrome("a") is True
+
+def test_8():
+    assert is_palindrome("черное небо") is False
+# pytest -v
