4964e822e5276a252394f8a82be5e4dc0d8e789b
Макс <svetachapa1963@gmail.com>
2025-11-23 18:29:41 +0300

    Слияние словарей

--- lab5.2/task5.py
+++ lab5.2/task5.py
@@ -1,1 +1,11 @@
+def combine_dicts(dict1: dict, dict2: dict) -> dict:
+#Принимает два словаря и делает новый словарь
+#содержащий все элементы из обоих, сохраняя порядок
+#Если ключи совпадают, значение берётся из второго словаря
+    result = {}
+    for key, value in dict1.items():    #добавляет элементы первого словаря
+        result[key] = value
+    for key, value in dict2.items():    #добавляет элементы второго словаря
+        result[key] = value
+    return result
 
