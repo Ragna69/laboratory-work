798fcad4a9f10c11fe3c2eca8809599b7ca663a4
Макс <svetachapa1963@gmail.com>
2025-12-23 12:04:03 +0300

    Убрал лишние else/finally

--- lab5.2/deposit/test_10_percent.py
+++ lab5.2/deposit/test_10_percent.py
@@ -7,10 +7,6 @@ def test_basic_deposit():
         assert years_deposit(5, 1354) == 2180.63
     except Exception as e:
         pytest.fail(f"Ошибка {e}")
-    else:
-        pass
-    finally:
-        pass
 
 def test_zero_deposit():
     try:
@@ -18,37 +14,24 @@ def test_zero_deposit():
         assert years_deposit(1234, 0) == 0.00
     except Exception as e:
         pytest.fail(f"Ошибка {e}")
-    else:
-        pass
-    finally:
-        pass
 
 def test_negative_value():
     try:
         years_deposit(-2, 1345)
         years_deposit(2, -1345)
     except ValueError as e:
-        assert "не могут быть отрицательными" in str(e)   # исправлено
+        assert "не могут быть отрицательными" in str(e)
     else:
         pytest.fail("Ожидалось исключение")
-    finally:
-        pass
-
 
 @pytest.fixture
 def deposit_data():
     return [(1000, 10, 2593.74),(3000, 30, 52348.21), (1354, 5, 2180.63)]
 
-def text_fixture(deposit_data):
-    years, amount, expected = deposit_data
-    try:
+def test_fixture(deposit_data):
+    for amount, years, expected in deposit_data:
         result = years_deposit(years, amount)
-    except Exception as e:
-        pytest.fail(f"Ошибка {e}")
-    else:
         assert result == expected
-    finally:
-        pass
 
 @pytest.mark.parametrize("years, amount, expected",[(1,1000, 1100.00),(2, 1000, 1210.00)])
 def test_parametrize(years, amount, expected):
@@ -58,17 +41,11 @@ def test_parametrize(years, amount, expected):
         pytest.fail(f"Ошибка {e}")
     else:
         assert result == expected
-    finally:
-        pass
 
 @pytest.mark.skip(reason="Пропуск")
 def test_skip_deposit():
     try:
         assert years_deposit(7, 500) == 973.50
-    except Exception:
-        pass
-    else:
-        pass
     finally:
         pass
 
@@ -80,9 +57,13 @@ def test_deposit_fail():
         raise
     else:
         pytest.fail("Ожидалась ошибка")
-    finally:
-        pass
 
+@pytest.mark.new(reason="Пропуск")
+def test_basic_deposit():
+    try:
+        assert years_deposit(3, 1000) == 1331.00
+    except Exception as e:
+        pytest.fail(f"Ошибка {e}")
 
 
 
